96d0afb157ad43b329e56733e78a8c7bbdd501ea
Nathanael <nathanael.rayapin@gmail.com>
2026-02-10 13:14:59 +0100

    feat: Shut down added and trigger cron job on start

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,25 @@
+import { startHealthcheckScheduler, stopHealthcheckScheduler } from "./cron/scheduler";
+
+// Start Cron Job
+const cronJob = startHealthcheckScheduler();
+
 const server = Bun.serve({
   port: 3000,
-  routes: {}
+  fetch(_req) {
+    return new Response('Healthcheck scheduler is running 🚀');
+  },
 });
 
 console.log(`Listening on ${server.url}`);
+
+// Stop Cron Job
+function shutdown(signal: string) {
+  console.log(`\n🛑 Received ${signal}, shutting down...`);
+
+  stopHealthcheckScheduler(cronJob);
+
+  server.stop?.();
+  process.exit(0);
+}
+
+process.on("SIGINT", shutdown);
