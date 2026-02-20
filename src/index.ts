9dfdf0315a9d8d4c2b315cc0bb9e3d2cfddf3a2c
Nathanael <nathanael.rayapin@gmail.com>
2026-02-20 11:51:51 +0100

    feat: Index updated

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,35 @@
+import { version } from '../package.json';
 /**
  * API Health Monitor
  * 
  * This package allows you to monitor the health status of multiple APIs
  * using Atlassian's standard Statuspage.io API.
  * 
- * @packageDocumentation
  */
 
-// Exports main functions
+export const API_VERSION = version;
+
+// Main functions
 export { fetchApiStatus } from './cron/fetch-status';
+export { fetchIncidents, fetchAllIncidents, countActiveIncidents, getMostCriticalIncident } from './cron/fetch-incidents';
 export { startHealthcheckScheduler, stopHealthcheckScheduler, runImmediateHealthcheck, } from './cron/scheduler';
 
-// Data exports
-export { APIS_TO_MONITOR } from './data/apis-to-monitor';
+// Datas
+export { APIS_TO_MONITOR } from './data/apis-to-monitor.constant';
+export { STATUS_MAPPING } from './data/status-mapping.constant';
+export { INCIDENT_STATUS, INCIDENT_IMPACT, ACTIVE_STATUSES, RESOLVED_STATUSES, IMPACT_PRIORITY } from './data/incident-constants';
+export { HTTP_ERROR_MAPPING, DEFAULT_HTTP_ERROR } from './data/http-error-mapping.constant';
+
+// Utilities
 export { getApisByCategory, getApiById, getApisByIds, } from './utils/apis';
-
-export { STATUS_MAPPING } from './data/status-mapping';
-export { HTTP_ERROR_MAPPING, DEFAULT_HTTP_ERROR } from './data/http-error-mapping';
-
-// Exports of utilities
 export { mapHttpStatus } from './utils/map-http-status';
 export { isApiStatus, isHttpError } from './utils/type-guards';
 export { getGravityIcon, getHttpErrorIcon } from './utils/icons';
 
-// Config exports
+// Configs
 export { loadConfig, saveConfig, deleteConfig, configExists, getConfigPath, } from './config/user-config';
 
-// Type exports
+// Interfaces
 export type { IApiConfig } from './data/apis-to-monitor.interface';
 export type { IIndicator } from './data/status-mapping.interface';
 export type { ISchedulerOptions } from './cron/scheduler.interface';
@@ -34,7 +37,8 @@ export type { IConfigAnswers, IPromptOptions } from './cli/prompts.interface';
 export type { IFetchStatusResult } from './cron/fetch-status.interface';
 export type { IHttpErrorStatus } from './interfaces/http-error-status';
 export type { IStatusResponse, IStatusIndicator } from './interfaces/status-json';
-export type { IIncidentsResponse, Incident, IncidentUpdate } from './interfaces/incidents-json';
+export type { IIncidentsResponse, IIncident, IIncidentUpdate } from './interfaces/incidents-json';
 export type { IPageInfo } from './interfaces/page-info-json';
 export type { IUserConfig } from './config/user-config.interface';
-export type { StatusValue } from './data/status-mapping';
+export type { StatusValue } from './data/status-mapping.constant';
+export type { IncidentStatus, IncidentImpact, IncidentFilterType } from './data/incident-constants';
